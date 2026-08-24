f91c5e53b15fb411f696bc10e0c2569a5d772dc7
ckouffsyrup <ckouffsyrup@gmail.com>
2026-08-23 21:38:01 -0500

    Fix native updater and prepar Reson v0.26.4

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -887,6 +887,8 @@ pub fn run() {
         .manage(Arc::new(Mutex::new(AudioEngine::default())))
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             backend_status,
             scan_music_folder,
